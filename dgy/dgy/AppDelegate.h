6cee27f021889dde859ad5a390e38c63795c8f31
zhang <123@qq.com>
2016-06-27 00:27:31 +0800

    first code init

---
+++ dgy/dgy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dgy
+//
+//  Created by mac on 16/6/27.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dgy/dgy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dgy
+//
+//  Created by mac on 16/6/27.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
